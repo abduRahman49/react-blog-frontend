ac6d68c44538f68b2341987015e3c46d433b2761
abdouseye <abdou.seye@gsietechnology.com>
2024-10-17 18:35:20 +0000

    Added CI config variable to app

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -27,7 +27,7 @@ const ArticlePage = () => {
         if(isLoading) {
             loadArticleInfo();
         }
-    }, [isLoading, user])
+    }, [isLoading, user, articleId])
     
     const article = articles.find(article => article.name === articleId);
 
